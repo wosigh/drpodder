bc5611b1b18e826b85978d1b1a706a477ea94da7
drnull <jdhatfield@gmail.com>
2009-07-30 23:15:14 -0400

    Adding powerService for startActivity (to keep wireless connected?)

--- bin/app/assistants/app-assistant.js
+++ bin/app/assistants/app-assistant.js
@@ -5,4 +5,5 @@ AppAssistant.prototype.setup = function() {
 	AppAssistant.downloadService = new DownloadService();
 	AppAssistant.mediaService = new MediaDBService();
 	AppAssistant.applicationManagerService = new ApplicationManagerService();
+	AppAssistant.powerService = new PowerService();
 };
